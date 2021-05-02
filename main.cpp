2457653986d8368323c60a81d709bba7074fd220
Adam-Vandervorst <adam.vandervorst@gmail.com>
2021-05-02 23:02:25 +0200

    Implement a tiny vector library.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include "test_all.h"
+#include "src/Vector.h"
 
 int main() {
-    test_all();
+    Vector<4> v({1., 2., 3., 4.});
+
+    std::cout << (v/(1. - v.one_hot(2))).bounds().second << std::endl;
+
+    std::cout << (v | v) << std::endl;
     return 0;
 }
